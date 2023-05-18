07ead2c639d2457d010c378e58489d9d9436c63e
Nitish Raj <jnraj4321@gmail.com>
2023-05-18 11:03:13 +0530

    added test cases

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { legacy_createStore as createStore } from 'redux';
-import { applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-import rootReducer from "./reducers/rootReducer";
+import store from './store';
 
 import Main from "./Main"; // Update the import statement
 
-const middleware = [thunk];
-const store = createStore(rootReducer, compose(applyMiddleware(...middleware)));
 
 
 const rootElement = document.getElementById("root");
